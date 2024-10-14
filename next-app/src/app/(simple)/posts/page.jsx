2fa19f48e05d9aa54876d0fd20dc48deb5afd22b
RioMarie <riomariejunio23@gmail.com>
2024-10-14 16:38:11 +0800

    feat: add data in post display

--- next-app/src/app/(simple)/posts/page.jsx
+++ next-app/src/app/(simple)/posts/page.jsx
@@ -1,9 +1,14 @@
+"use client"
+
 import React from 'react'
 import PostsPage from '../../pages/posts/page'
+import { PostProvider } from '../../../../context/post-context'
 
 const Posts = () => {
   return (
-    <PostsPage />
+    <PostProvider>
+      <PostsPage />
+    </PostProvider>
   )
 }
 
--- next-app/views/posts.jsx
+++ next-app/views/posts.jsx
@@ -1,10 +1,24 @@
+import { useContext } from "react"
+import { PostProvider, PostContext } from "../context/post-context"
+import Link from "next/link";
+
 const Posts = () => {
+  const posts = useContext(PostContext); // Access the post data from context
 
   return (
     <div className="container">
-
-    </div>
-  )
-}
+      <h1>our posts</h1>
+      <ul className="cardList">
+          {posts.map(post => (
+              <Link href={`/posts/${post.id}`} key={post.id}>
+                <li key={post.id} className="card pointer">
+                  <h2 className="text-bold">{post.title}</h2>
+                  <p>{post.content}</p>
+                </li>
+              </Link>
+          ))}
+      </ul>
+  </div>
+)};
 
 export default Posts
