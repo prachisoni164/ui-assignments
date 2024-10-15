f02cb436ff81c14098774359e265e1f3e073a285
RioMarie <riomariejunio23@gmail.com>
2024-10-15 13:22:27 +0800

    feat: add style for active nav item

--- next-app/components/NavBar.jsx
+++ next-app/components/NavBar.jsx
@@ -1,29 +1,34 @@
-import Link from 'next/link'
-import React from 'react'
+"use client";
+
+import Link from 'next/link';
+import React from 'react';
+import { usePathname } from 'next/navigation';
 import '../styles/nav.styles.css'; 
 
 const NavBar = () => {
+  const pathname = usePathname(); // Get current route
+
   return (
     <nav>
       <h1 className="logo cursor-pointer">
         <Link href="/">next-app</Link>
       </h1>
       <ul className="nav-list">
-        <li className="nav-item">
+        <li className={`nav-item ${pathname.includes('/services') ? 'active' : ''}`}>
           <Link href="/services">services</Link>
         </li>
-        <li className="nav-item">
+        <li className={`nav-item ${pathname.includes('/posts') ? 'active' : ''}`}>
           <Link href="/posts">posts</Link>
         </li>
-        <li className="nav-item">
+        <li className={`nav-item ${pathname.includes('/about') ? 'active' : ''}`}>
           <Link href="/about">about</Link>
         </li>
-        <li className="nav-item">
+        <li className={`nav-item ${pathname.includes('/contact') ? 'active' : ''}`}>
           <Link href="/contact">contact</Link>
         </li>
       </ul>
-  </nav>
-  )
-}
+    </nav>
+  );
+};
 
-export default NavBar
+export default NavBar;
