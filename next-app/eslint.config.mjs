19dc1af391c7ae16fb35dd01db9abc3f4a6afec9
RioMarie <riomariejunio23@gmail.com>
2024-10-16 10:16:38 +0800

    chore: modify eslint config

--- next-app/eslint.config.mjs
+++ next-app/eslint.config.mjs
@@ -2,10 +2,15 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginReact from "eslint-plugin-react";
 
-
 export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  pluginReact.configs.flat.recommended,
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    languageOptions: {
+      globals: globals.browser, 
+    },
+    rules: {
+      ...pluginJs.configs.recommended.rules, 
+      ...pluginReact.configs.flat.recommended.rules, 
+    },
+  },
 ];
