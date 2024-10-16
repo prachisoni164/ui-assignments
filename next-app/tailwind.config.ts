063766e227d796cae61fd90592e992867c98235e
RioMarie <riomariejunio23@gmail.com>
2024-10-16 15:08:01 +0800

    chore: update tailwind config

--- next-app/tailwind.config.ts
+++ next-app/tailwind.config.ts
@@ -3,6 +3,7 @@ const config = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./views/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {
