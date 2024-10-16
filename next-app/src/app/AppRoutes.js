e2e94ee5849a66449803171253868eed1044b7c4
RioMarie <riomariejunio23@gmail.com>
2024-10-16 09:49:12 +0800

    feat: add app routes for user and product

---
+++ next-app/src/app/AppRoutes.js
@@ -0,0 +1,15 @@
+export const PRODUCT_ROUTES = {
+    GET_ALL_PRODUCTS: '/products',
+    GET_PRODUCT: '/products/',          
+    CREATE_PRODUCT: '/products/add',
+    UPDATE_PRODUCT: '/products/',
+    DELETE_PRODUCT: '/products/',
+}
+
+export const USER_ROUTES = {
+    GET_ALL_USERS: '/users',
+    GET_USER: '/users/',
+    CREATE_USER: '/users/add',
+    UPDATE_USER: '/users/',
+    DELETE_USER: '/users/',
+}
