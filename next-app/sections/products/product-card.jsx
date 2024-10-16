edec54685f8608bd70087eab3d791276499aeca6
RioMarie <riomariejunio23@gmail.com>
2024-10-16 15:09:21 +0800

    feat: fetch all products route

--- next-app/sections/products/product-card.jsx
+++ next-app/sections/products/product-card.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import Image from 'next/image';
+import styles from "./product-card.module.css"
 
-const ProductCard = () => {
-  return (
-    <div>product-card</div>
-  )
+const ProductCard = (product) => {
+
+    return (
+        <>
+            <div className={styles.productCard}>
+                <Image
+                    src={product.product.thumbnail}
+                    alt={product.product.name}
+                    width={50}
+                    height={50}
+                    layout="responsive"
+                />
+                <h2 className={styles.productTitle}>{product.product.title}</h2>
+                <p className={styles.productDescription}>{product.product.description}</p>
+            </div>
+        </>
+            
+        
+    )
 }
 
 export default ProductCard
--- next-app/views/products/products.jsx
+++ next-app/views/products/products.jsx
@@ -1,13 +1,46 @@
-import React, { useContext } from "react"
+"use client"
+
+import React, { useState, useEffect } from 'react';
 import ProductCard from "../../sections/products/product-card";
+import { get } from "../../src/app/HttpClient"
+import { PRODUCT_ROUTES } from '../../src/app/AppRoutes'
+import "../../src/app/globals.css"
 
 const Products = () => {
- 
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetchProducts(); 
+  },[]);
+
+  const fetchProducts = async () => {
+    try {
+      const response = await get(`${PRODUCT_ROUTES.GET_ALL_PRODUCTS}?limit=10&skip=0`)
+      setProducts(response.data.products);
+    }
+    catch (err) {
+      setError('Failed to fetch products'); // Handle any error that occurs
+    } 
+    finally {
+      setLoading(false); // Set loading to false after fetching is complete
+    }
+  }
+
+  if (loading) return <div className="container"><p className='text-center'>Loading...</p></div>; // Show loading state
+  if (error) return <div>{error}</div>; // Show error message
 
   return (
     <div className="container">
-      <h1>our products</h1>
-      <ProductCard />
+      <h1 className='mt-5'>our products</h1>
+      <div className='flex flex-wrap gap-2 justify-center'>
+        {products.map((product) => {
+          return (
+            <ProductCard key={product.id} product={product} />
+          )
+        })}
+      </div>
     
   </div>
 )};
