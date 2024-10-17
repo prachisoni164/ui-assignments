071f881e76a92bfe29863817fb4d3f22f11fea3c
RioMarie <riomariejunio23@gmail.com>
2024-10-17 12:46:35 +0800

    fix: fix destructure of product

--- next-app/sections/products/product-card.jsx
+++ next-app/sections/products/product-card.jsx
@@ -3,25 +3,25 @@ import Image from 'next/image';
 import styles from "./product-card.module.css"
 import { useRouter } from 'next/navigation';
 
-const ProductCard = (product) => {
+const ProductCard = ({product}) => {
     const router = useRouter();
 
     const handleCardClick = () => {
-      router.push(`/products/${product.product.id}`);
+      router.push(`/products/${product.id}`);
     };
 
     return (
         <>
             <div className={styles.productCard} onClick={handleCardClick}>
                 <Image
-                    src={product.product.thumbnail}
-                    alt={product.product.name}
+                    src={product.thumbnail}
+                    alt={product.name}
                     width={50}
                     height={50}
                     layout="responsive"
                 />
-                <h2 className={styles.productTitle}>{product.product.title}</h2>
-                <p className={styles.productDescription}>{product.product.description}</p>
+                <h2 className={styles.productTitle}>{product.title}</h2>
+                <p className={styles.productDescription}>{product.description}</p>
             </div>
         </>
             
