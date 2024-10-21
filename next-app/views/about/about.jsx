73e9e45b09d39788e7f8065ae07a82a1249703c7
RioMarie <riomariejunio23@gmail.com>
2024-10-21 17:12:12 +0800

    feat: add mui components in post page

--- next-app/views/about/about.jsx
+++ next-app/views/about/about.jsx
@@ -15,7 +15,7 @@ const About = () => {
       <Typography variant="h4" sx={{ my: 3, textAlign: "center" }} gutterBottom>
         About Us
       </Typography>
-      <Typography variant="body1">
+      <Typography variant="body1" color="textSecondary">
         Welcome to our company! We are dedicated to providing exceptional
         services and solutions that cater to the needs of our clients. Our
         mission is to deliver innovative and effective strategies that empower
@@ -24,7 +24,7 @@ const About = () => {
       <Typography variant="h4" sx={{ my: 3, textAlign: "center" }} gutterBottom>
         Our Team
       </Typography>
-      <Typography variant="body1">
+      <Typography variant="body1" color="textSecondary">
         Our team consists of highly skilled professionals with diverse
         backgrounds and expertise. We are passionate about what we do and
         committed to delivering the best outcomes for our clients.
--- next-app/views/posts/post-by-id.jsx
+++ next-app/views/posts/post-by-id.jsx
@@ -1,29 +1,46 @@
-import React, { useContext } from "react"
-import { PostContext } from "../../context/post-context"
+import React, { useContext } from "react";
+import { PostContext } from "../../context/post-context";
+import { Box, Typography } from "@mui/material";
 
 const PostById = (id) => {
-    const posts = useContext(PostContext); // Access the post data from context
+  const posts = useContext(PostContext); // Access the post data from context
 
-    const postId = id.id; // Access the dynamic route parameter
-    
-    // Find the post with the specific id
-    const post = posts.find((post) => post.id === parseInt(postId)); 
+  const postId = id.id; // Access the dynamic route parameter
 
+  // Find the post with the specific id
+  const post = posts.find((post) => post.id === parseInt(postId));
 
-    return (
-        <div className="full-container">
-            <div>
-                {post ? (
-                    <>
-                        <h1>{post.title}</h1>
-                        <p>{post.content}</p>
-                    </>
-                ) : (
-                    <p>Post not found.</p>
-                )}
-            </div>
-        </div>
-    );
+  return (
+    <Box
+      height="75vh"
+      width="60%"
+      margin="auto"
+      padding={2}
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      {post ? (
+        <>
+          <Typography
+            variant="h4"
+            sx={{ my: 3, textAlign: "left" }}
+            gutterBottom
+          >
+            {post.title}
+          </Typography>
+          <Typography variant="body1" color="textSecondary">
+            {post.content}
+          </Typography>
+        </>
+      ) : (
+        <p>Post not found.</p>
+      )}
+    </Box>
+  );
 };
 
 export default PostById;
